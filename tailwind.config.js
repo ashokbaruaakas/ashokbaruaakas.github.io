a626c8250cb454b6da17e89936e99ad7f56249aa
Ashok Barua <ashokbaruaakas@gmail.com>
2020-10-27 23:06:44 +0600

    Header section simple fix for LG screen

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        "header-bg": "20rem", // Header BG Height for LG
+      },
+      margin: {
+        "-header": "-9.1rem", // Header section top margin  for LG
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
