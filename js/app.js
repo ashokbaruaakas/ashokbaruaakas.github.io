84308cdd5da3ad9476dc70bcd9e934ef1c9ff42d
ashokbaruaakas <ashokbaruaakas@gmail.com>
2020-11-19 22:23:37 +0600

    Simple page animation added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+// Smooth scrolling
 new SmoothScroll('a[href*="#"]', {
   speed: 800,
   easing: 'easeInOutQuad'
 });
+
+// Animating page
+ScrollReveal({
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+  duration: 800,
+  scale: 0.25,
+  opacity: 0,
+  delay: 200,
+  origin: 'bottom',
+});
+ScrollReveal().reveal('#header')
+ScrollReveal().reveal('#about', {delay: 500})
+ScrollReveal().reveal('#contact')
