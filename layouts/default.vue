6d4bf6c6022dcccacbabad28344b08324b3e612e
Ashok <ashokbaruaakas@gmail.com>
2024-04-13 16:32:36 +0600

    ✨ Add SEO metadata to default layout component.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: "Ashok | Software Engineer",
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "title", content: "Ashok | Software Engineer Portfolio" },
+    {
+      name: "description",
+      content:
+        "Welcome to the portfolio of Ashok, a passionate Software Engineer specializing in PHP, Laravel, Vue, TypeScript and more. Explore my projects and experience in software development.",
+    },
+  ],
+});
+</script>
 
 <template>
   <div>
